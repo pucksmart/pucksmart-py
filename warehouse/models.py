1d52da1247faed52ff757ac4102b464b4c320f80
Brian Devins-Suresh <badevins@gmail.com>
2024-03-27 16:55:24 -0400

    Load players into database

--- warehouse/models.py
+++ warehouse/models.py
@@ -9,3 +9,19 @@ class HttpSource(models.Model):
     e_tag = models.CharField(max_length=64, null=True, blank=True)
     last_refreshed = models.DateTimeField()
     blob_path = models.CharField(max_length=256)
+
+
+class NhlPlayer(models.Model):
+    nhl_id = models.IntegerField(unique=True)
+    given_name = models.CharField(max_length=64)
+    family_name = models.CharField(max_length=64)
+    is_active = models.BooleanField(default=False)
+    position = models.CharField(max_length=2)
+    hall_of_fame = models.BooleanField(default=False)
+    birth_date = models.DateField()
+    birth_locality = models.CharField(max_length=64)
+    birth_region = models.CharField(max_length=64, null=True, blank=True)
+    birth_country = models.CharField(max_length=3)
+    handedness = models.CharField(max_length=1)
+    height = models.IntegerField(null=True, blank=True)
+    weight = models.IntegerField(null=True, blank=True)
--- warehouse/tasks.py
+++ warehouse/tasks.py
@@ -1,12 +1,15 @@
 from datetime import timezone, timedelta, datetime
+from io import StringIO
+from itertools import product
+from string import ascii_lowercase
 
 import boto3
 import requests
+import pandas as pd
 from botocore.config import Config
 from celery import shared_task
 
-from warehouse.models import HttpSource
-
+from warehouse.models import HttpSource, NhlPlayer
 
 s3 = boto3.resource("s3",
                     endpoint_url="http://localhost:9000",
@@ -34,7 +37,7 @@ def load_play_by_play(game_id: int):
             r = requests.get(source.url, allow_redirects=True)
             source.e_tag = r.headers.get("ETag")
             source.last_refreshed = datetime.now(timezone.utc)
-            source.blob_path = "game/%d/play-by-play" % game_id
+            source.blob_path = "source/game/%d/play-by-play" % game_id
             s3.Object("pucksmart", source.blob_path).put(Body=r.content)
     source.save()
 
@@ -56,6 +59,96 @@ def load_shifts(game_id: int):
             r = requests.get(source.url, allow_redirects=True)
             source.e_tag = r.headers.get("ETag")
             source.last_refreshed = datetime.now(timezone.utc)
-            source.blob_path = "game/%d/shifts" % game_id
+            source.blob_path = "source/game/%d/shifts" % game_id
             s3.Object("pucksmart", source.blob_path).put(Body=r.content)
     source.save()
+
+
+def _get_player_ids_from_boxscore(boxscore):
+    player_ids = []
+    for p in boxscore["playerByGameStats"]["awayTeam"]["forwards"]:
+        player_ids.append(p["playerId"])
+    for p in boxscore["playerByGameStats"]["awayTeam"]["defense"]:
+        player_ids.append(p["playerId"])
+    for p in boxscore["playerByGameStats"]["awayTeam"]["goalies"]:
+        player_ids.append(p["playerId"])
+    for p in boxscore["summary"]["gameInfo"]["awayTeam"]["scratches"]:
+        player_ids.append(p["id"])
+    for p in boxscore["playerByGameStats"]["homeTeam"]["forwards"]:
+        player_ids.append(p["playerId"])
+    for p in boxscore["playerByGameStats"]["homeTeam"]["defense"]:
+        player_ids.append(p["playerId"])
+    for p in boxscore["playerByGameStats"]["homeTeam"]["goalies"]:
+        player_ids.append(p["playerId"])
+    for p in boxscore["summary"]["gameInfo"]["homeTeam"]["scratches"]:
+        player_ids.append(p["id"])
+    return player_ids
+
+
+@shared_task
+def load_boxscore(game_id: int):
+    url = "https://api-web.nhle.com/v1/gamecenter/%d/boxscore" % game_id
+    try:
+        source = HttpSource.objects.get(url=url)
+    except HttpSource.DoesNotExist:
+        source = HttpSource(url=url)
+
+    if not source.last_refreshed or source.last_refreshed + timedelta(days=1) < datetime.now(timezone.utc):
+        e_tag = ""
+        if source.e_tag:
+            r = requests.head(source.url, allow_redirects=True)
+            e_tag = r.headers.get("ETag")
+        if not source.e_tag or e_tag != source.e_tag:
+            r = requests.get(source.url, allow_redirects=True)
+            source.e_tag = r.headers.get("ETag")
+            source.last_refreshed = datetime.now(timezone.utc)
+            source.blob_path = "source/game/%d/boxscore" % game_id
+            s3.Object("pucksmart", source.blob_path).put(Body=r.content)
+
+            players = _get_player_ids_from_boxscore(r.json())
+            load_player.map(players).apply_async()
+    source.save()
+
+
+@shared_task
+def load_player(nhl_id: int):
+    url = "https://api-web.nhle.com/v1/player/%d/landing" % nhl_id
+    try:
+        player = NhlPlayer.objects.get(nhl_id=nhl_id)
+    except NhlPlayer.DoesNotExist:
+        player = NhlPlayer(nhl_id=nhl_id)
+
+    player_summary = requests.get(url, allow_redirects=True).json()
+    player.given_name = player_summary["firstName"]["default"]
+    player.family_name = player_summary["lastName"]["default"]
+    player.is_active = player_summary["isActive"]
+    player.position = player_summary["position"]
+    player.hall_of_fame = player_summary["inHHOF"]
+    player.birth_date = player_summary["birthDate"]
+    player.birth_locality = player_summary["birthCity"]["default"]
+    if "birthStateProvince" in player_summary:
+        player.birth_region = player_summary["birthStateProvince"]["default"]
+    player.birth_country = player_summary["birthCountry"]
+    player.handedness = player_summary["shootsCatches"]
+    player.height = player_summary["heightInCentimeters"]
+    player.weight = player_summary["weightInKilograms"]
+
+    player.save()
+
+
+@shared_task
+def load_players():
+    search_terms = map("".join, product(ascii_lowercase, repeat=3))
+    players = []
+    for search_term in search_terms:
+        url = "https://search.d3.nhle.com/api/v1/search/player?culture=en-us&limit=1000&q=" + search_term
+        r = requests.get(url)
+        print(search_term)
+        players = players + r.json()
+        print(len(players))
+    df = pd.DataFrame(players)
+    print(df.shape)
+
+    csv_buffer = StringIO()
+    df.to_csv(csv_buffer)
+    s3.Object("pucksmart", "source/players.csv").put(Body=csv_buffer.getvalue())
